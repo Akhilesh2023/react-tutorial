937db4db53ff9b750bf73a329642c8c12d9e505a
Akhilesh rana <akhileshrana2023@gmail.com>
2023-05-10 11:06:41 +0530

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+console.log("React file is running");
+//JSX-JavaScript XML
+//var template = <p>This is JSX from app.js! </p>;
+
+var template = React.createElement("h1", { id: "someid" }, "Something New");
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
